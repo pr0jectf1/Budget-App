d117a9023fb32d3d34838b4733b7ecc3bd5f1adf
Luis <ldg4life95@yahoo.com>
2024-05-24 16:20:09 -0400

    Introduced and embedded thje CredtDebt type.

--- BudgetApp.go
+++ BudgetApp.go
@@ -2,11 +2,18 @@ package main
 
 import "fmt"
 
+type CreditDebt struct {
+	CreditorName string
+	Amount	     float64
+}
+
 type User struct {
 	
 	Id int
 	FirstName string
 	LastName string
+	CombinedCreditDebt []CreditDebt
+	
 }
 
 func createNewUser(Id int, firstName, lastName string) User {

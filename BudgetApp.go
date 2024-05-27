7a5f1a278c3bea6fd094180f34162a725c023c80
Luis <ldg4life95@yahoo.com>
2024-05-27 15:23:43 -0400

    Seperated functionality of creating a new user and adding new debt for possible reuse in the future and improved readability.

--- BudgetApp.go
+++ BudgetApp.go
@@ -20,7 +20,71 @@ func createNewUser(Id int, firstName, lastName string) User {
 	return User{Id: Id, FirstName: firstName, LastName: lastName}
 }
 
+func (u User) addCreditDebt(creditDebt CreditDebt) {
+	
+	u.CombinedCreditDebt = append(u.CombinedCreditDebt, creditDebt)
+}
+
+func (u User) addUserDebt() {
+	var input string
+	var numInput int
+	var debtAmount float64
+
+	for{
+		fmt.Println("Hello", u.FirstName, "would you like to add any new debt? (y/n)", )
+		fmt.Scan(&input)
+
+		if input == "y" {
+			fmt.Println("How many different debts would you like to add? (Please enter a number)")
+			fmt.Scan(&numInput)
+
+			for i:= 0; i < numInput; i++ {
+
+				fmt.Print("Creditors name: ")
+				fmt.Scan(&input)
+				fmt.Print("Credit amount: ")
+				fmt.Scan(&debtAmount)
+				fmt.Print()
+
+				//check if debt exists in users list
+				// if the debt exists ask user if this is a new debt to the same creditor.
+				//if so, create new debt with different debtId
+				//if not, go on to next debt 
+		
+				creditDebt := CreditDebt{
+					CreditorName: input,
+					Amount: debtAmount,
+				}
+				u.addCreditDebt(creditDebt)
+			}
+			break
+		} else if input == "n" {
+			fmt.Printf("Wow %v, congrats on having no debt!\n\n", u.FirstName)
+			break
+		} else {
+			fmt.Println("Sorry, that is not a valid input. Please try again.")
+		}
+	}
+}
+
+func getNewUserDetails() User{
+	var firstName string
+	var lastName string
+
+	fmt.Println("Hello what's your first name?")
+	fmt.Scan(&firstName)
+	fmt.Println("What's your last name?")
+	fmt.Scan(&lastName)
+	newUser := createNewUser(1, firstName, lastName)
+
+	return newUser
+}
+
 func main() {
-	u1 := createNewUser(1, "Johnny", "Garcia")
-	fmt.Println(u1.FirstName)
+
+	u1 := getNewUserDetails()
+	u1.addUserDebt()
+
+	fmt.Printf("What would you like to do \n")
+	
 }
